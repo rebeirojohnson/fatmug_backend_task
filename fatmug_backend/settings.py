463aa1ae23b19e1068499c8a600d12f81ed28462
johnson <johnson@basics.fun>
2024-05-01 17:15:47 +0530

    loading the env file on start

--- fatmug_backend/settings.py
+++ fatmug_backend/settings.py
@@ -11,21 +11,29 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zci^qb3ee%f7v)@$mj24@&er0t*i9ekfeg5(%yruq$a)ojh4(4'
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = os.getenv(key='SECRET_KEY',default=get_random_secret_key())
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
