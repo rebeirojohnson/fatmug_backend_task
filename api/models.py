9ae0e35d55da37b54513bdb6c89ce58f39909778
johnson <johnson@basics.fun>
2024-05-01 18:42:40 +0530

    added get method

--- api/models.py
+++ api/models.py
@@ -5,19 +5,19 @@ from django.db import models
 class Vendor(models.Model):
     vendor_code = models.CharField(max_length=100,unique=True,auto_created=True,primary_key=True)
     name = models.CharField(max_length=100)
-    contact_details = models.TextField()
-    address = models.TextField()
-    on_time_delivery_rate = models.FloatField()
-    quality_rating_avg = models.FloatField()
-    average_response_time = models.FloatField()
-    fulfillment_rate = models.FloatField()
+    contact_details = models.TextField(null=True)
+    address = models.TextField(null=True)
+    on_time_delivery_rate = models.FloatField(null=True)
+    quality_rating_avg = models.FloatField(null=True)
+    average_response_time = models.FloatField(null=True)
+    fulfillment_rate = models.FloatField(null=True)
 
     def __str__(self):
         return self.name
 
 
 class PurchaseOrder(models.Model):
-    po_number = models.CharField(max_length=200)
+    po_number = models.CharField(max_length=200,primary_key=True,auto_created=True,unique=True)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     order_date = models.DateTimeField()
     delivery_date = models.DateTimeField()
--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-	path('', views.index_view, name="base"),
+	path('', views.VendorViews, name="base"),
+	path('vendors/', views.VendorViews, name="base"),
+	# path('vendors/<str:vendor_code>/', views.index_view, name="base"),
+	# path('purchase_orders/', views.index_view, name="base"),
+	# path('purchase_orders /<str:po_id>/', views.index_view, name="base"),
+	# path('', views.index_view, name="base"),
+	# path('', views.index_view, name="base"),
 ]
