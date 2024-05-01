1bca3484423637e016e9f8a342921e48376eb379
johnson <johnson@basics.fun>
2024-05-01 17:53:47 +0530

    added all the midesk

--- api/models.py
+++ api/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Vendor(models.Model):
+    vendor_code = models.CharField(max_length=100,unique=True,auto_created=True,primary_key=True)
+    name = models.CharField()
+    contact_details = models.TextField()
+    address = models.TextField()
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class PurchaseOrder(models.Model):
+    po_number = models.CharField(max_length=200)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    order_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    status = models.CharField()
+    quality_rating = models.FloatField()
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class HistoricalPerformance(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+
+
+    def __str__(self):
+        return self.title
