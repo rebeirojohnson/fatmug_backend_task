db3d6fa13c2e6bf31b199da518cb23de211179b2
johnson <johnson@basics.fun>
2024-05-02 15:14:37 +0530

    added ackowledgment

---
+++ Testing/order_acknowledge.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+def view_purchase_ordeers(order_id):
+    
+
+    url = f"http://127.0.0.1:8000/api/purchase_orders/{order_id}/acknowledge/"
+
+    
+    list_of_vendors = requests.post(url=url).json()
+    
+    print(json.dumps(list_of_vendors,indent=4))
+    
+if __name__ == '__main__':
+    
+    order_id = 'f1e0f118-1714-45e5-876b-11b549060922' 
+    
+    view_purchase_ordeers(order_id = order_id)
