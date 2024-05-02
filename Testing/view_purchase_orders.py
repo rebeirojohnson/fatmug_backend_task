c40ee8a5f241ca508694eacc5206f8e497776f22
johnson <johnson@basics.fun>
2024-05-02 15:06:43 +0530

    added wmaple to view purchase order

---
+++ Testing/view_purchase_orders.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+def view_purchase_ordeers(vendor_code=None):
+    
+    if vendor_code:
+        url = "http://127.0.0.1:8000/api/purchase_orders/?vendor_code={vendor_code}"
+    else:
+        url = "http://127.0.0.1:8000/api/purchase_orders/"
+    
+    list_of_vendors = requests.get(url=url).json()
+    
+    print(json.dumps(list_of_vendors,indent=4))
+    
+if __name__ == '__main__':
+    
+    vendor_code = 'NSLZO' 
+    
+    vendor_code = None
+    
+    view_purchase_ordeers(vendor_code = vendor_code)
