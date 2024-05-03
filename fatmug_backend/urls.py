1525797113205e5fee56517e85e46ffb5c5d79f4
johnson <johnson@basics.fun>
2024-05-03 10:16:53 +0530

    added swagger docs

--- fatmug_backend/urls.py
+++ fatmug_backend/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.auth.views import LoginView, LogoutView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/",SpectacularSwaggerView.as_view(template_name="swagger-ui.html", url_name="schema"),name="swagger-ui",),
+
 ]
