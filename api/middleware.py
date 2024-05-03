ee0ee28d3c0632c9e202a62d82c625e6d8dd0bee
johnson <johnson@basics.fun>
2024-05-03 14:46:46 +0530

    testing readme

--- api/middleware.py
+++ api/middleware.py
@@ -11,7 +11,7 @@ class CustomAuthenticationMiddleware:
         
         is_authenticated = request.session.get('is_authenticated',False)
                
-        if not is_authenticated and not request.path == settings.LOGIN_URL:
+        if not is_authenticated and not request.path == settings.LOGIN_URL and not request.path == '/api/docs/':
             
             response = Response({"error": "Unauthorized"}, status=status.HTTP_401_UNAUTHORIZED)
             
